59e6207962ac8cda2257f1de295ab49ae4b76585
kazzi58 <kaziraiyan@iut-dhaka.edu>
2024-05-17 15:50:31 +0100

    Edit

--- data/tag_categories.js
+++ data/tag_categories.js
@@ -2,12 +2,6 @@ define({
     tagCategories: {
         "type": {
             "description": "Type of the paper"
-        },
-        "field": {
-            "description": "Field of research"
-        },
-        "input_data": {
-            "description": "Type of input data"
         }
         // Add more categories if needed
     }
